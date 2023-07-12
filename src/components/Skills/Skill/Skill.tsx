10c1aa6f28373c927625049b5408136f8d5e6599
Aleksey <easy.mail.to.copy@gmail.com>
2023-07-12 21:22:18 +0300

    Improved Projects Block

--- src/components/Skills/Skill/Skill.tsx
+++ src/components/Skills/Skill/Skill.tsx
@@ -10,7 +10,7 @@ export const Skill = ({ title, description }: SkillType) => {
     <div className={s.skillContainer}>
       <div className={s.skill}>
         <div className={s.iconBox}><img className={s.icon} src="https://img.icons8.com/?size=512&id=bzf0DqjXFHIW&format=png" alt="#" /></div>
-        <h3>{title}</h3>
+        <h3 className={s.title}>{title}</h3>
         <p className={s.description}>{description}</p>
       </div>
     </div>
